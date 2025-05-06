b15f0db3bdce1cde112eb1cfd289e19b31262a37
GenivaldoSerra <genivaldo.serra@live.com>
2025-05-06 15:51:15 -0300

    refactor: Ajuste no formato da DATA na resposta! 🥈

--- aula02/validador_dia_util.py
+++ aula02/validador_dia_util.py
@@ -1,3 +1,4 @@
+from altair import Align
 import streamlit as st
 from datetime import datetime
 
@@ -52,16 +53,17 @@ def main():
             key="data"
         )
         
-    col_btn1, col_btn2 = st.columns(2)
+    col_btn1, col_btn2  = st.columns(2)
     with col_btn1:
         verificar_btn = st.button("Verificar", use_container_width=True, key="verificar_btn")
 
     if verificar_btn:
+        data_formatada = data.strftime("%d/%m/%Y")
         dia_util = data.weekday() < 5
         if dia_util:
-            st.markdown(f'<div class="result-container util">O dia {data} é um dia útil.</div>', unsafe_allow_html=True)
+            st.markdown(f'<div class="result-container util">O dia {data_formatada} é um dia útil.</div>', unsafe_allow_html=True)
         else:
-            st.markdown(f'<div class="result-container nao_util">O dia {data} não é um dia útil.</div>', unsafe_allow_html=True)
+            st.markdown(f'<div class="result-container nao_util">O dia {data_formatada} não é um dia útil.</div>', unsafe_allow_html=True)
     
 
     st.markdown('<footer class="footer">© 2025 - Validador de Dia Útil</footer>', unsafe_allow_html=True)
